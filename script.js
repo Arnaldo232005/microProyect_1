a4e2d264b026e8a3c4aca54e16e55e6a068bca88
Arnaldo232005 <arnaldo.vallerugo@correo.unimet.edu.ve>
2025-02-15 18:26:50 -0400

    listo el juego en su totalidad

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*Variables enlazadas con el html y los audios*/
 const cuadros = document.querySelectorAll('.cuadro');
 const listo= document.getElementById('listo');
 const ronda=document.getElementById('ronda');
@@ -7,18 +8,19 @@ const c3=new Audio('sol-101774.mp3');
 const c4=new Audio('re-78500.mp3');
 
 
-
+/*Variables del juego*/
 let patron = [];
 let posicion = [];
 let numRonda = 0;
 let turno = false;
 
-function getRandomColor() {
-    const colors = ['uno', 'dos', 'tres', 'cuatro'];
-    return colors[Math.floor(Math.random() * 4)];
+/*Obtener un cuadro aleatorio*/
+function seleccionarColor() {
+    const color = ['uno', 'dos', 'tres', 'cuatro'];
+    return color[Math.floor(Math.random() * 4)];
 }
-
-function flashButton(color) {
+/*Activar boton*/
+function encenderBoton(color) {
     const button = document.getElementById(color);
     button.classList.add('patron-activo');
     if(color==='uno'){
@@ -41,19 +43,19 @@ function flashButton(color) {
         button.classList.remove('patron-activo');
     }, 550);
 }
-
-function flashButtonClick(color) {
+/*Activar click*/
+function encenderClick(color) {
     const button = document.getElementById(color);
     button.classList.add('click-activo');
     setTimeout(() => {
         button.classList.remove('click-activo');
     }, 350);
 }
-
-function playSequence() {
+/*Mostrar patron al usuario*/
+function reproducirPatron() {
     let i = 0;
     const interval = setInterval(() => {
-        flashButton(patron[i]);
+        encenderBoton(patron[i]);
         i++;
         if (i >= patron.length) {
             clearInterval(interval);
@@ -63,41 +65,41 @@ function playSequence() {
         }
     }, 745);
 }
-
-function nextLevel() {
+/*Entrar a la siguiente ronda*/
+function siguienteRonda() {
     numRonda++;
     ronda.textContent=`Ronda ${numRonda}`;
     turno = false;
     posicion = [];
-    patron.push(getRandomColor());
+    patron.push(seleccionarColor());
     setTimeout(() => {
-        playSequence();
+        reproducirPatron();
     }, 900);
 }
-
-function resetGame() {
+/*Devolver valores iniciales*/
+function reiniciarJuego() {
     patron = [];
     posicion = [];
     numRonda = 0;
     turno= false;
 }
-
-function handlePlayerInput(color) {
+/*Comprobar aciertos*/
+function comprobar(color) {
     if (!turno) return;
     posicion.push(color);
-    flashButtonClick(color);
+    encenderClick(color);
     const currentStep = posicion.length - 1;
 
     if (posicion[currentStep] !== patron[currentStep]) {
         setTimeout(() => {
-            resetGame();
+            reiniciarJuego();
         }, 500);
         window.location.href='inicio.html';
     } else {
         if (posicion.length === patron.length) {
             turno = false;
             setTimeout(() => {
-                nextLevel();
+                siguienteRonda();
             }, 1000);
         }
     }
@@ -106,11 +108,11 @@ function handlePlayerInput(color) {
 cuadros.forEach(button => {
     button.addEventListener('click', (e) => {
         const color = e.target.id;
-        handlePlayerInput(color);
+        comprobar(color);
     });
 });
 
 listo.addEventListener('click', () => {
-    resetGame();
-    nextLevel();
+    reiniciarJuego();
+    siguienteRonda();
 });
